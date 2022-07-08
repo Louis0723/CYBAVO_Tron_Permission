adac18cd7b0bc99e82f02bd9e8ca262e1aa2cbdf
Louis_0723 <lursun@cybavo.com>
2022-07-08 09:11:12 +0800

    update color

--- src/App.js
+++ src/App.js
@@ -16,14 +16,14 @@ function Type(props) {
     for (let i = 0; i < addressCount; i++) {
       result.push(
         <div key={i}>
-          <input type="text" className="rounded w-8/12" placeholder={`${typeName} address`}
+          <input type="text" className="rounded w-8/12 border-rose-500" placeholder={`${typeName} address`}
             onChange={(e) => {
               typeResult[i] = {
                 ...typeResult[i], ...{ address: window.tronWeb.utils.base58.decode58(e.target.value).map(s => s.toString(16).padStart(2, '0')).join('').substr(0, 42) }
               }
               setTypeResult([...typeResult])
             }} />
-          <input type="number" className="rounded w-4/12" placeholder={`${typeName} weight`}
+          <input type="number" className="rounded w-4/12 border-rose-500" placeholder={`${typeName} weight`}
             onChange={(e) => {
               typeResult[i] = {
                 ...typeResult[i], ...{ weight: Number(e.target.value) }
@@ -98,7 +98,7 @@ function Active(props) {
     props.callback(active)
   }, [active])
   return (
-    <div className='inline-block shadow-xl ring-1 ring-gray-900/5  sm:p-10 sm:m-5'>
+    <div className='inline-block bg-amber-50 shadow-xl ring-1 ring-gray-900/5  sm:p-10 sm:m-5'>
       <Type typeName='active' count={1} callback={(address, threshold) => {
         active.keys = address
         setActive({ ...active, threshold })
@@ -124,6 +124,7 @@ function App() {
   const [activeCount, setActiveCount] = useState(1)
   const [permissionData, setPermissionData] = useState({ actives: [], owner: { type: 0 }, witness: { type: 1 }, visible: false })
   const actives = []
+
   for (let i = 0; i < activeCount; i++) {
     actives.push(<Active key={i} callback={(active) => {
       active.permission_name = `active${i}`
@@ -136,7 +137,7 @@ function App() {
   console.log(permissionData)
 
   return (
-    <div className="relative flex min-h-screen flex-col  overflow-hidden bg-gray-50 py-6 sm:py-12">
+    <div className="relative min-h-screen flex flex-col  overflow-hidden bg-gray-50 py-6 sm:py-12">
       <div className="absolute inset-0 bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
       <div className="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto  sm:rounded-lg sm:px-10">
         <div className="mx-auto ">
@@ -174,16 +175,22 @@ function App() {
             <div>
               <button className='px-4 py-2 m-1 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm'
                 onClick={() => {
-                  (activeCount + 1 <= 8) && setActiveCount(activeCount + 1)
+                  permissionData.actives.length = activeCount + 1
+                  setPermissionData({ ...permissionData })
+                    (activeCount + 1 <= 8) && setActiveCount(activeCount + 1)
                 }}
               >+</button>
               <button className='px-4 py-2 m-1 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm'
                 onClick={() => {
-                  (activeCount - 1 >= 0) && setActiveCount(activeCount - 1)
+                  permissionData.actives.length = activeCount - 1
+                  setPermissionData({ ...permissionData })
+                    (activeCount - 1 >= 0) && setActiveCount(activeCount - 1)
                 }}
               >-</button>
             </div>
-            {actives}
+            <div className='flex justify-start items-start'>
+              {actives}
+            </div>
           </div>
         </div>
       </div>
